088c7a41d7908364f69f00774590ad216c95aeab
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2024-12-06 15:52:25 -0800

    Initial Commit

---
+++ AnswersAi/AnswersAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnswersAiApp.swift
+//  AnswersAi
+//
+//  Created by Daniel Berezhnoy on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AnswersAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnswersAi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnswersAi
+//
+//  Created by Daniel Berezhnoy on 12/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
