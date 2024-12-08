b8641ff595a33c7aad3a88bc98b04176e1625c03
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2024-12-08 01:31:38 -0800

    Save

--- AnswersAi/Screens/AppDetails.swift
+++ AnswersAi/Screens/AppDetails.swift
@@ -10,26 +10,17 @@ import SwiftUI
 struct AppDetails: View {
     let card: Card
     
-    private let bannerHeight: CGFloat = 400
-    
-    
     let text = "Reach your pinnacle as a runner with Runna: Running Training Plans, your quintessential guide to the world of running. With tailored training plans built by world-class coaches and powered by Al to help you achieve your goals. \n\nEmbark on an unparalleled running journey with Runna. More than just a pocket-sized running coach, Runna is your comprehensive running companion. From beginners embarking on their first park run to seasoned athletes gearing up for challenging marathons, our app is crafted to cater to all. Our offering includes training for 5k, 10k, half-marathons, marathons, and ultra marathons. This is probably why we boast a stellar 4.9/5 rating on Trustpilot."
     
     var body: some View {
         VStack(spacing: 0) {
             imageBanner
-            
             appDetails
             Spacer()
-            
 //            Text(text)
         }
         .ignoresSafeArea()
-        
 //        .navigationBarBackButtonHidden()
-//        .frame(height: bannerHeight + 95)
-        
-//        .navigationBarBackButtonHidden(viewVisible)
     }
     
     @State private var viewVisible = false
@@ -47,7 +38,7 @@ struct AppDetails: View {
         Image(.peopleRunning)
             .resizable()
             .scaledToFill()
-            .frame(height: bannerHeight)
+            .frame(height: 400)
 //            .clipped()
     }
     
@@ -111,7 +102,5 @@ struct AppDetails: View {
 }
 
 #Preview {
-//    NavigationStack {
-        AppDetails(card: Card())
-//    }
+    AppDetails(card: Card())
 }
