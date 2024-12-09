880dc6c207406303699f1f8aa3c35fbc9c2cc125
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2024-12-08 16:21:16 -0800

    Save

--- AnswersAi/Model/Card.swift
+++ AnswersAi/Model/Card.swift
@@ -9,30 +9,30 @@ import SwiftUI
 
 struct Card: Identifiable {
     let id = UUID()
+    let banner: CardBanner = .mockCover
+    let appDetails: CardAppDetails = .mockAppDetails
 }
 
-// MARK: CardCover
-// Header
-// Title
-// Subtitle
-// CoverImage
-
-// MARK: CardAppDetails
-// AppName
-// AppTitle
-// AppDescription
-// AppIcon
-
-struct CardCover {
+// MARK: CardBanner
+struct CardBanner {
     let header: String
     let title: String
     let subtitle: String
     let coverImage: Image
 }
 
+extension CardBanner {
+    static let mockCover = CardBanner(header: "Answers", title: "AI answers", subtitle: "Your questions, answered", coverImage: Image("answers-ai"))
+}
+
+// MARK: CardAppDetails
 struct CardAppDetails {
     let appName: String
     let appSubtitle: String
     let appDescription: String
     let appIcon: Image
+}
+
+extension CardAppDetails {
+    static let mockAppDetails = CardAppDetails(appName: "Answers", appSubtitle: "AI answers", appDescription: "Your questions, answered", appIcon: Image("answers-ai"))
 }
