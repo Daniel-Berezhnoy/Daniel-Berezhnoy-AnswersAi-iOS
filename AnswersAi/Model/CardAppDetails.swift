3550a3ba4828f5854d3c9e896b58882185b83a58
Daniel Berezhnoy <43868541+Daniel-Berezhnoy@users.noreply.github.com>
2024-12-08 22:46:16 -0800

    Save

--- AnswersAi/Model/CardAppDetails.swift
+++ AnswersAi/Model/CardAppDetails.swift
@@ -10,15 +10,15 @@ import SwiftUI
 struct CardAppDetails {
     let name: String
     let subtitle: String
-    let description: String
     let icon: Image
+    let description: String
 }
 
 extension CardAppDetails {
     static let runnaAppDetails = CardAppDetails(
         name: "Runna",
         subtitle: "RunBuddy",
-        description: "Reach your pinnacle as a runner with Runna: Running Training Plans, your quintessential guide to the world of running. With tailored training plans built by world-class coaches and powered by Al to help you achieve your goals. \n\nEmbark on an unparalleled running journey with Runna. More than just a pocket-sized running coach, Runna is your comprehensive running companion. From beginners embarking on their first park run to seasoned athletes gearing up for challenging marathons, our app is crafted to cater to all. Our offering includes training for 5k, 10k, half-marathons, marathons, and ultra marathons. This is probably why we boast a stellar 4.9/5 rating on Trustpilot.",
-        icon: Image(.appIcon)
+        icon: Image(.appIcon),
+        description: "Reach your pinnacle as a runner with Runna: Running Training Plans, your quintessential guide to the world of running. With tailored training plans built by world-class coaches and powered by Al to help you achieve your goals. \n\nEmbark on an unparalleled running journey with Runna. More than just a pocket-sized running coach, Runna is your comprehensive running companion. From beginners embarking on their first park run to seasoned athletes gearing up for challenging marathons, our app is crafted to cater to all. Our offering includes training for 5k, 10k, half-marathons, marathons, and ultra marathons. This is probably why we boast a stellar 4.9/5 rating on Trustpilot."
     )
 }
